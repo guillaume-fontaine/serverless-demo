6d522ca72da4c0da63a864665605d6bd945206ea
bstocker <boris.stocker@gmail.com>
2025-03-26 11:43:19 +0100

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,94 @@
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    lambda     = "http://ip10-0-6-5-cvhtafqb9qb14bivkpqg-4566.direct.lab-boris.fr"
+    apigateway = "http://ip10-0-6-5-cvhtafqb9qb14bivkpqg-4566.direct.lab-boris.fr"
+    iam        = "http://ip10-0-6-5-cvhtafqb9qb14bivkpqg-4566.direct.lab-boris.fr"
+    dynamodb   = "http://ip10-0-6-5-cvhtafqb9qb14bivkpqg-4566.direct.lab-boris.fr"
+  }
+}
+
+resource "aws_dynamodb_table" "contacts" {
+  name         = "contacts"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "email"
+
+  attribute {
+    name = "email"
+    type = "S"
+  }
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_lambda_function" "api" {
+  function_name = "hello-api"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "handler.handler"
+  runtime       = "python3.9"
+  filename      = "${path.module}/lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/lambda.zip")
+
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.contacts.name
+    }
+  }
+}
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "hello-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.api.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "hello_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "contact_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /contact"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
