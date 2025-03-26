fa82b21f48ee5b1c5f03511c616c1cbf13437bdc
bstocker <boris.stocker@gmail.com>
2025-03-26 13:01:55 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
+  token                       = ""
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
