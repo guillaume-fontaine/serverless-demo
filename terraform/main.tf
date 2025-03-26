6d00a1d375096b3705fd024782e448387f3bfc22
bstocker <boris.stocker@gmail.com>
2025-03-26 13:43:06 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,24 +15,13 @@ provider "aws" {
   }
 }
 
-resource "aws_dynamodb_table" "contacts" {
-  name         = "contacts"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "email"
-
-  attribute {
-    name = "email"
-    type = "S"
-  }
-}
-
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role"
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
       Principal = {
         Service = "lambda.amazonaws.com"
       }
@@ -42,54 +31,58 @@ resource "aws_iam_role" "lambda_exec" {
 
 resource "aws_lambda_function" "api" {
   function_name = "hello-api"
-  role          = aws_iam_role.lambda_exec.arn
   handler       = "handler.handler"
   runtime       = "python3.9"
-  filename      = "${path.module}/lambda.zip"
-  source_code_hash = filebase64sha256("${path.module}/lambda.zip")
+  role          = aws_iam_role.lambda_exec.arn
+  filename      = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
+}
 
-  environment {
-    variables = {
-      TABLE_NAME = aws_dynamodb_table.contacts.name
-    }
+resource "aws_dynamodb_table" "contacts" {
+  name         = "contacts"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
   }
 }
 
-resource "aws_apigateway_rest_api" "rest_api" {
+resource "aws_api_gateway_rest_api" "api" {
   name        = "hello-api"
-  description = "API REST simulée dans LocalStack"
+  description = "API REST simulée"
 }
 
-resource "aws_apigateway_integration" "lambda_integration" {
-  api_id                 = aws_apigatewayv2_api.http_api.id
-  integration_type       = "AWS_PROXY"
-  integration_uri        = aws_lambda_function.api.invoke_arn
-  integration_method     = "POST"
-  payload_format_version = "2.0"
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "hello"
 }
 
-resource "aws_apigateway_route" "hello_route" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "GET /hello"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
 }
 
-resource "aws_apigateway_route" "contact_route" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "POST /contact"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.proxy.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api.invoke_arn
 }
 
-resource "aws_apigateway_stage" "default" {
-  api_id      = aws_apigatewayv2_api.http_api.id
-  name        = "$default"
-  auto_deploy = true
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  depends_on = [aws_api_gateway_integration.lambda]
 }
 
-resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.api.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigateway_api.http_api.execution_arn}/*/*"
+resource "aws_api_gateway_stage" "stage" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "dev"
 }
