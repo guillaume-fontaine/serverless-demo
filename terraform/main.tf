7f36a803002c386dee8ead86ab55264f70de5f19
bstocker <boris.stocker@gmail.com>
2025-03-26 16:42:20 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,10 +8,10 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    lambda     = "http://ip10-0-6-5-cvhtafqb9qb14bivkpqg-4566.direct.lab-boris.fr"
-    apigateway = "http://ip10-0-6-5-cvhtafqb9qb14bivkpqg-4566.direct.lab-boris.fr"
-    iam        = "http://ip10-0-6-5-cvhtafqb9qb14bivkpqg-4566.direct.lab-boris.fr"
-    dynamodb   = "http://ip10-0-6-5-cvhtafqb9qb14bivkpqg-4566.direct.lab-boris.fr"
+    lambda     = "http://ip10-0-7-4-cvi159ib9qb14bivkpt0-4566.direct.lab-boris.fr"
+    apigateway = "http://ip10-0-7-4-cvi159ib9qb14bivkpt0-4566.direct.lab-boris.fr"
+    iam        = "http://ip10-0-7-4-cvi159ib9qb14bivkpt0-4566.direct.lab-boris.fr"
+    dynamodb   = "http://ip10-0-7-4-cvi159ib9qb14bivkpt0-4566.direct.lab-boris.fr"
   }
 }
 
@@ -30,13 +30,12 @@ resource "aws_iam_role" "lambda_exec" {
 }
 
 resource "aws_lambda_function" "api" {
-  function_name    = "hello-api"
-  handler          = "handler.handler"
-  runtime          = "python3.9"
-  role             = aws_iam_role.lambda_exec.arn
-  filename         = "lambda.zip"
+  function_name = "hello-api"
+  handler       = "handler.handler"
+  runtime       = "python3.9"
+  role          = aws_iam_role.lambda_exec.arn
+  filename      = "lambda.zip"
   source_code_hash = filebase64sha256("lambda.zip")
-
   environment {
     variables = {
       TABLE_NAME = aws_dynamodb_table.contacts.name
@@ -82,94 +81,101 @@ resource "aws_api_gateway_integration" "lambda" {
   uri                     = aws_lambda_function.api.invoke_arn
 }
 
-# Contact endpoint
-resource "aws_api_gateway_resource" "contact" {
+resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "contact"
+  depends_on = [aws_api_gateway_integration.lambda]
 }
 
-resource "aws_api_gateway_method" "contact_post" {
+resource "aws_api_gateway_stage" "stage" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.contact.id
-  http_method   = "POST"
+  stage_name    = "dev"
+}provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  token                       = ""
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    lambda     = "http://ip10-0-7-4-cvi159ib9qb14bivkpt0-4566.direct.lab-boris.fr"
+    apigateway = "http://ip10-0-7-4-cvi159ib9qb14bivkpt0-4566.direct.lab-boris.fr"
+    iam        = "http://ip10-0-7-4-cvi159ib9qb14bivkpt0-4566.direct.lab-boris.fr"
+    dynamodb   = "http://ip10-0-7-4-cvi159ib9qb14bivkpt0-4566.direct.lab-boris.fr"
+  }
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_lambda_function" "api" {
+  function_name = "hello-api"
+  handler       = "handler.handler"
+  runtime       = "python3.9"
+  role          = aws_iam_role.lambda_exec.arn
+  filename      = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.contacts.name
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "contacts" {
+  name         = "contacts"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "hello-api"
+  description = "API REST simulée"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "hello"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "contact_lambda" {
+resource "aws_api_gateway_integration" "lambda" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.contact.id
-  http_method             = aws_api_gateway_method.contact_post.http_method
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.proxy.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.api.invoke_arn
 }
 
-# CORS SUPPORT -----------------------------
-resource "aws_api_gateway_method" "options" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.proxy.id
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "options" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.options.http_method
-  type        = "MOCK"
-
-  request_templates = {
-    "application/json" = <<EOF
-{
-  "statusCode": 200
-}
-EOF
-  }
-}
-
-resource "aws_api_gateway_integration_response" "options" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.options.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'*'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-
-  response_templates = {
-    "application/json" = ""
-  }
-}
-
-resource "aws_api_gateway_method_response" "options" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.options.http_method
-  status_code = "200"
-
-  response_models = {
-    "application/json" = "Empty"
-  }
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
-}
-# ------------------------------------------
-
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  depends_on = [
-    aws_api_gateway_integration.lambda,
-    aws_api_gateway_integration.options,
-    aws_api_gateway_integration.contact_lambda
-  ]
+  depends_on = [aws_api_gateway_integration.lambda]
 }
 
 resource "aws_api_gateway_stage" "stage" {
