d3120754cd19afe53455c1abf8d5e502c06b0f25
Guillaume Fontaine <50714231+guillaume-fontaine@users.noreply.github.com>
2025-04-03 10:58:29 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -111,6 +111,15 @@ resource "aws_api_gateway_integration" "contact" {
   uri                     = aws_lambda_function.api.invoke_arn
 }
 
+resource "aws_api_gateway_integration" "contact_get" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.contact.id
+  http_method             = aws_api_gateway_method.contact_get.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api.invoke_arn
+}
+
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
 
